b880e2a8d7d338e87e008db4bdab32046a7b94b7
John Bowen <john@bowencode.net>
2015-03-16 20:08:26 -0400

    Creating initial project

---
+++ QuickCalc/QuickCalc/ViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace QuickCalc
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		public ICommand NumberPress { get; set; }
+
+		private string _numberDisplay = "0";
+
+		public string NumberDisplay
+		{
+			get { return _numberDisplay; }
+			set
+			{
+				if (_numberDisplay == value)
+					return;
+				_numberDisplay = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
